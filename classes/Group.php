4a63538162fa2a839e8f52c2d66945f029de3937
DPetrushin <petrushin_denis@mail.ru>
2017-04-01 13:06:36 +0300

    Все методы + пример

---
+++ classes/Group.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Group
+ *
+ * @author petru
+ */
+class Group {
+
+    var $id;
+    var $name;
+    var $screen_name;   
+    var $type;
+    var $photo_200="";
+    var $members_count;
+    var $members=array();
+    function __construct(/* $response */) {
+        /* РїР°СЂСЃРёС‚СЊСЃСЏ РёР· response */
+    }
+
+}
---
+++ classes/MembersOfGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+class MembersOfGroup {
+
+    var $id;
+    var $first_name;
+    var $last_name;
+    var $city;
+    var $photo_50;
+    function __construct() {
+        
+    }
+
+}
