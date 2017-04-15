4864d8ad286d16b3feeba4f6de77f02a9e17e3e4
DPetrushin <petrushin_denis@mail.ru>
2017-04-15 13:27:50 +0500

    Add files via upload
    xml,json

--- parsers/parser.php
+++ parsers/parser.php
@@ -1,5 +1,5 @@
-<?php
-
+﻿<?php
+setlocale (LC_CTYPE, "ru_RU.UTF-8");
 require 'classes/Users.php';
 require 'classes/Group.php';
 require 'classes/MembersOfGroup.php';
@@ -37,7 +37,7 @@ class pars {
             }
         }
         $user->relation = Users::$rel[$user->relation];
-        pars::GetGroupByUser($user, 0, $access_token);
+        pars::GetGroupByUser($user, 0, $access_token);       
         return $user;
     }
 
@@ -151,8 +151,35 @@ class pars {
         return $member;
     }
 
-    static function LoadGroup() {
-        
+    static function ObjecttoJSON($object){
+        return json_encode($object,JSON_UNESCAPED_UNICODE);
+    }
+    
+    static function JSONtoXML($json) {
+        $xml=new SimpleXMLElement('<data/>');
+        pars::arrayToXml($json, $xml);
+        return html_entity_decode($xml->asXML(),ENT_QUOTES, 'utf-8');
+    }
+    static function arrayToXml($array,&$xml){
+        foreach ($array as $key => $value) {
+            if(is_array($value) || is_object($value)){
+                pars::arrayToXml($value, $xml->addChild($key));
+            }else{
+                $xml->addChild($key, htmlspecialchars($value));
+            }
+        }        
+    }
+    
+    static function JSONtoStr($json,&$result){
+        foreach ($json as $key => $value) {
+            if(is_array($value) || is_object($value)){
+                $result.='Æ';
+                pars::JSONtoStr($value, $result);
+                $result=$result.'Æ';
+            }else{
+                $result.=$key.':'.$value.'Ћ';
+            }
+        }
     }
 
 }
